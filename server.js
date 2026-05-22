5bd12109042a2f73bd3f3d0ea15e8d86aeca24ad
turkovdima79 <turkovdima79@gmail.com>
2026-05-22 17:28:55 +0300

    add Registration

--- server.js
+++ server.js
@@ -6,17 +6,83 @@ const pathToIndex = path.join(__dirname, "static", "index.html");
 const indexHtmlFile = fs.readFileSync(pathToIndex);
 
 const scriptFile = fs.readFileSync(path.join(__dirname, "static", "script.js"));
+
 const styleFile = fs.readFileSync(path.join(__dirname, "static", "style.css"));
+const registerFile = fs.readFileSync(path.join(__dirname, "static", "register.html"));    
+const authFile = fs.readFileSync(path.join(__dirname, 'static', 'auth.js'));
+
+const db = require("./database");
+
+
 
 
 const server = http.createServer((req, res) => {
-  switch(req.url){
-   case "/": return res.end(indexHtmlFile);
-   case "/script.js": return res.end(scriptFile);
-   case "/style.css": return res.end(styleFile);
-
+  if(req.method === 'GET') {
+    switch(req.url) {
+      case '/': return res.end(indexHtmlFile);
+      case '/script.js': return res.end(scriptFile);
+      case '/auth.js': return res.end(authFile);
+      case '/style.css': return res.end(styleFile);
+      case '/register.html': return res.end(registerFile);
+    }
   }
-  res.statusCode = 404; 
-  return res.end("Eror 404");
+  if(req.method === 'POST') {
+    switch(req.url) {
+      case '/api/register': return registerUser(req, res);
+    }
+  }
+  return res.end('Error 404');
 });
+
+
+
+function registerUser(req, res) {
+    let data = '';
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', async function() {
+      try {
+        const user = JSON.parse(data);
+        if(!user.login || !user.password) {
+          return res.end('Empty login or password');
+        }
+        if(await db.isUserExist(user.login)) {
+          return res.end('User already exist');
+        }
+        await db.addUser(user);
+        return res.end('Registeration is successfull');
+      }
+      catch(e) {
+        return res.end('Error: ' + e);
+      }
+    });
+}
+
+
+
 server.listen(3000);
+
+
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", async (socket) => {
+    console.log("a user connected. id = " + socket.id);
+
+    let userNickName = "admin";
+
+    // Надіслати історію при вході
+    let messages = await db.getMessages();
+    socket.emit("all_messages", messages);
+
+    socket.on("new_message", async (message) => {
+        await db.addMessage(message, 1);
+
+        // Оновити список після запису
+        let updatedMessages = await db.getMessages();
+
+        io.emit("all_messages", updatedMessages);
+    });
+});
--- static/script.js
+++ static/script.js
@@ -1,1 +1,37 @@
-alert("Welcome to PersonalChat!");  
+const socket = io();
+
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (input.value) {
+        socket.emit('new_message', input.value);
+        input.value = '';
+    }
+});
+
+// Отримання всіх повідомлень
+socket.on('all_messages', function(allMessages) {
+    messages.innerHTML = '';
+
+    allMessages.forEach(msg => {
+        const item = document.createElement('li');
+        item.textContent = `${msg.login}: ${msg.content}`;
+        messages.appendChild(item);
+    });
+
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+function changeNickname() {
+    let nickname = prompt('Choose your nickname');
+
+    if (nickname) {
+        socket.emit('set_nickname', nickname);
+    }
+}
+
+changeNickname();
